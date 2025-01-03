2ddfa6821640e6f5382e36b85646dae48713caa0
BAKKALIAYOUB <ayoubbakkkali484@gmail.com>
2025-01-03 16:48:03 +0100

    add docker ignore

--- usage_example.py
+++ usage_example.py
@@ -3,7 +3,7 @@ from config.setting import settings
 from conversation_agents.ollama_agent import ConversationAgent
 
 mymodel = MyOllamaModel(model_name=settings.OLLAMA_MODEL)
-agent = ConversationAgent(sessionid="skhdskjdsdnsd", model=mymodel)
+agent = ConversationAgent(sessionid="your session id", model=mymodel)
 
-response = agent.generate_response("d'accord merci beacoup")
+response = agent.generate_response("Your question")
 print(response)
